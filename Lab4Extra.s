e62441455068f6a697247aef32c3b1d0e37c4f6c
pauldehm <38186931+pauldehm@users.noreply.github.com>
2018-04-08 12:17:32 -0400

    Pauls Comments 4/8/18
    Added some comments for readability

--- Lab4Extra.s
+++ Lab4Extra.s
@@ -1,4 +1,4 @@
-	AREA |.text|, CODE, READONLY 
+AREA |.text|, CODE, READONLY 
 	EXPORT __main
 	ENTRY
 
@@ -8,16 +8,16 @@ __main
 	MOV R1, #0xFF
 	STR R1, [R0]		;Set direction of all 8 pins to OUT
 	MOV R5, #0x1
-	MOV R6, #0 		;Flashing mode parameter
+	MOV R6, #0 			;Flashing mode parameter
 	MOV R3, #0x800
 	MOV R8, #0xFF
 	MOV R9, #0
 	B main_loop
 
 delay
-	MOV R4, #0			; int R4;
+	MOV R4, #0			;int R4;
 delay_loop	
-	ADD R4, R4, #1		; for(R4 = 0; R4 < R1; R4++);
+	ADD R4, R4, #1		;for(R4 = 0; R4 < R1; R4++);
 	CMP R4, R1	
 	BLT delay_loop
 	MOV PC, LR 			;return
@@ -28,20 +28,20 @@ mode1					;LEDs flash left to right
 	BL delay			;delay(6000000)		
 	STR R5, [R2]		;turn off LED - write 0 to FIOPIN
 	BL delay
-	LSL R5,R5,#1
+	LSL R5,R5,#1		;shift the flashing LED to the left
 	CMP R5, #0x100
-	MOVEQ R5,#0x1
+	MOVEQ R5,#0x1		;reset flashing LED to the rightmost LED
 	B main_loop
 	
-mode2					; LEDs flash from right to left
+mode2					;LEDs flash from right to left
 	STR R5, [R2] 		;turn on LED - write 1 to FIOPIN
 	LDR R1, =1000000
 	BL delay			;delay(6000000)		
 	STR R5, [R2]		;turn off LED - write 0 to FIOPIN
 	BL delay
-	LSR R5,R5,#1
+	LSR R5,R5,#1		;shift the flashing LED to the right
 	CMP R5, #0x0
-	MOVEQ R5,#0x80
+	MOVEQ R5,#0x80		;reset flashing LED to leftmost LED
 	B main_loop
 	
 mode3
@@ -63,7 +63,7 @@ mode3_set2
 	MOVEQ R9, #0
 	LSL R5,R5,#1
 mode3_end
-	B main_loop
+	B main_loop			;return to the main loop
 	
 mode4
 	MOV R8, #0xFF
@@ -73,15 +73,15 @@ mode4
 	MOV R8, #0x00
 	STR R8, [R2]		;turn off LED - write 0 to FIOPIN
 	BL delay
-	B main_loop
+	B main_loop			;return to the main loop
 
-main_loop
-	LDR R7, [R2]
-	AND R7, R7, R3
-	CMP R7, #0x0
-	ADDEQ R6, R6, #1
-	CMP R6, #4
-	MOVEQ R6, #0
+main_loop				;essentially the main loop of the program
+	LDR R7, [R2]		;check pushbutton value
+	AND R7, R7, R3		;mask pushbutton address with mask value
+	CMP R7, #0x0		;check to see if pushbutton is pressed
+	ADDEQ R6, R6, #1	;increment the mode to the next mode
+	CMP R6, #4			;check to see if the mode has reached the final mode
+	MOVEQ R6, #0		;reset mode value to 0
 	CMP R6,#0
 	BEQ mode1
 	CMP R6, #1
